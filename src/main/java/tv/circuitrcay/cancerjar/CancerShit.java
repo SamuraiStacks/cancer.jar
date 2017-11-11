df8264a1bee653d06595437a522b9a41b55badc0
CircuitRCAY <dmanrazor@hotmail.com>
2017-11-11 15:12:25 +1100

    rickrolled

--- src/main/java/tv/circuitrcay/cancerjar/CancerShit.java
+++ src/main/java/tv/circuitrcay/cancerjar/CancerShit.java
@@ -7,23 +7,26 @@ import java.io.IOException;
 import java.lang.reflect.Constructor;
 
 public class CancerShit {
-    // From Natan#1289
     public static Unsafe createUnsafe() throws Exception {
         Constructor<Unsafe> ctor = Unsafe.class.getDeclaredConstructor();
         ctor.setAccessible(true);
         return ctor.newInstance();
     }
-    // From Skweezi Leekz#2426
     public static void spamSucc() {
         for(int i=0; i<1000000; i++) {
             System.out.println("FREE SUCC!!!");
         }
     }
-    // From Kodehawa#3457
     public static void notepad() throws IOException {
         Runtime runtime = Runtime.getRuntime();
         for(int i=0; i<100; i++) {
             Process process = runtime.exec("notepad %0|%0");
+        }
+    }
+    public static void rickroll() throws IOException{
+        Runtime runtime = Runtime.getRuntime();
+        for(int i=0; i<10; i++) {
+            Process process = runtime.exec("chrome https://www.youtube.com/watch?v=oHg5SJYRHA0");
         }
     }
 }
--- src/main/java/tv/circuitrcay/cancerjar/Console.java
+++ src/main/java/tv/circuitrcay/cancerjar/Console.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 
 public class Console {
    public static void main(String[] args) {
-       System.out.println("Cancer.jar - the original");
+        System.out.println("Cancer.jar - the original");
+        run();
+   }
+   private static void run() {
        Scanner scanner = new Scanner(System.in);
        String s = scanner.nextLine();
        if(s.equals("help")) {
            String help = "All the cancerous commands:\n"
                    + "succ - spams \"FREE SUCC\"\n"
                    + "jvm - OOM's JVM\n"
-                   + "notepad - surprise";
+                   + "notepad - surprise\n"
+                   + "rickroll - you already know";
            System.out.println(help);
        } if(s.equals("jvm")) {
            try {
@@ -21,14 +25,27 @@ public class Console {
                e.printStackTrace();
            }
        } if(s.equals("succ")) {
-            CancerShit.spamSucc();
+           CancerShit.spamSucc();
        } if (s.equals("notepad")) {
            try {
                CancerShit.notepad();
            } catch (IOException e) {
                e.printStackTrace();
            }
+       } if(s.equals("credits")) {
+           String credits = "Credits go to: \n"
+                   + "Natan#1289 - the idea of the \"jvm\" command \n"
+                   + "Kodehawa#3457 - extended idea of the \"notepad\" command \n"
+                   + "Skweezi Leekz#2426 - the idea of the \"succ\" command \n"
+                   + "Github: https://github.com/weirddogdev/cancer.jar";
+           System.out.println(credits);
+       } if(s.equals("rickroll")) {
+            try {
+                CancerShit.rickroll();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
        }
-       main(args);
+       run();
    }
 }
