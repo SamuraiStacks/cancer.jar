145db526a586e1c93036e52e7d596c8c0a50bea5
CircuitRCAY <dmanrazor@hotmail.com>
2017-11-13 08:45:39 +1100

    sa'in

--- src/main/java/tv/circuitrcay/cancerjar/Console.java
+++ src/main/java/tv/circuitrcay/cancerjar/Console.java
@@ -91,9 +91,9 @@ public class Console {
                System.out.println("Gr8 b8, m8. I rel8, str8 appreci8, and congratul8. I r8 this b8 an 8/8. Plz no h8, I’m str8 ir8. Cre8 more, can’t w8. We should convers8, I won’t ber8, my number is 8888888, ask for N8. No calls l8 or out of st8. If on a d8, ask K8 to loc8. Even with a full pl8, I always have time to communic8 so don’t hesit8");
            } else {
                System.out.println("Ain't a number between 1-5 you twat!");
-               return;
            }
        }
+
        run();
    }
 }
